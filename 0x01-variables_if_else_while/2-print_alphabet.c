036b6eb77e93655015382cde4f13c663ae9002bc
Mohammed Nagdy <monagdym@gmail.com>
2022-12-11 12:53:56 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+
+/**
+ * * Prorgram to print lowercase alphabet
+ * * Return char alphabet
+ * */
+
+int main(void)
+{
+	int alph=97; 
+	for (alph; alph<123; alph++)
+		printf("%c", alph);
+	printf("\n");
+
+	return (0);
+}
+
