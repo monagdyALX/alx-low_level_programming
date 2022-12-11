c21148cf9eb2ae368679a22a16f3c9f1b2ba732d
monagdyALX <119080152+monagdyALX@users.noreply.github.com>
2022-12-11 03:07:09 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
   else if ((n%10) == 0)
     printf("Last digit of %d is %d and is 0\n",  n, (n%10));
   else if (abs(n%10) < 6 && abs(n%10) > 0 )
-    printf("Last digit of %d is $d and is less than 6 and not 0\n",  n, (n%10));
+    printf("Last digit of %d is %d and is less than 6 and not 0\n",  n, (n%10));
     
 	return (0);
 }
