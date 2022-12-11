b448d61be33189a8d07817bad34e06b98477f0fa
monagdyALX <119080152+monagdyALX@users.noreply.github.com>
2022-12-11 23:13:38 +0200

    Update 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the 0-9 in single digit,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+  for (dig =0; dig < 10; dig++)
+	{
+		printf("%d", dig);
+	}
+
+	putchar('\n');
+	return (0);
+}
