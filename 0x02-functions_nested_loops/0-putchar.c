b0985b73bd8e0e414c0b06b2984b5c428eac4a37
Mohammed Nagdy <monagdym@gmail.com>
2022-12-14 13:54:25 -0800

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the 0-9 in single digit,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
