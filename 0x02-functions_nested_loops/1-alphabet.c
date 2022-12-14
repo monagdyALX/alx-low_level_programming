844de3d1c8519427ccbf5268a78890bd7fde8e73
Mohammed Nagdy <monagdym@gmail.com>
2022-12-14 14:32:43 -0800

    all created

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_alphabet - prints lower alphabet,
- * followed by a new line
- */
-void print_alphabet(){
-	  char i;
-	  for(i = 'a'; i<='z'; i++)
-	        putchar(i);
-	  putchar('\n');
-}
-
-/**
- * main - prints the 0-9 in single digit,
- * followed by a new line
- * Return: Always 0 (Success)
- */
-int main(void)
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
 }
