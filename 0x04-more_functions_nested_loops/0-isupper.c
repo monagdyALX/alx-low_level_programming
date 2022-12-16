1d14abfc29cc8e106ccfa995aa2230e70fbee3d4
monagdyALX <119080152+monagdyALX@users.noreply.github.com>
2022-12-16 20:36:39 +0200

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checks whether the letter is cap or not,
+ * followed by a new line
+ * Return: 1 for Caps 0 for Small
+ */
+int _isupper(char c)
+{
+  if(c <= 90 && c >= 60)
+  {
+    return(1);
+  }
+  return(0);
+}
+
+/**
+ * main - prints the 0-9 in single digit,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+  c = 'A';
+  printf("%c: %d\n", c, _isupper(c));
+  c = 'a';
+  printf("%c: %d\n", c, _isupper(c));
+  return (0);
+}
