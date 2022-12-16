438414355c9dd59522b48e397848a9c7a78955e8
monagdyALX <119080152+monagdyALX@users.noreply.github.com>
2022-12-16 20:45:38 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _isupper - checks whether the letter is cap or not,
- * followed by a new line
- * Return: 1 for Caps 0 for Small
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
 int _isupper(int c)
 {
-  if(c <= 90 && c >= 60)
-  {
-    return(1);
-  }
-  return(0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
 }
